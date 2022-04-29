2116f5827927f8c04a40591dfc101c450774cf7f
Alex Diego <alex.diegosn10@gmail.com>
2022-04-29 03:22:05 +0000

    global styles

---
+++ src/GlobalStyle.js
@@ -0,0 +1,35 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+export const colors = {
+  primary: '#F71A2C',
+  secondary: '#0D0D0D',
+  background: '#000',
+  text: '#fff'
+}
+
+const GlobalStyle = createGlobalStyle`
+  :root {
+    font-size: 62.5%;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    border: none;
+  }
+  
+  body {
+    width: 100%;
+    background-color: ${colors.background};
+    color: ${colors.text};
+    font-family: 'Barlow', sans-serif;
+  }
+`;
+
+export const Container = styled.div`
+  width: 70%;
+  margin: 0 auto;
+`;
+
+export default GlobalStyle;
--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
+import GlobalStyle from './GlobalStyle';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <App />
+      <GlobalStyle />
   </React.StrictMode>
 )
